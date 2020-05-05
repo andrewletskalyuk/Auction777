4a94a432dc4884cbf5d198dd274cf969b4db7e78
letskalyuk <andrewletskalyuk@gmail.com>
2020-05-05 21:33:28 +0300

    SellerWindow

--- AuctionClient/SellerWindow.xaml.cs
+++ AuctionClient/SellerWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AuctionClient.ServiceReference1;
+using AuctionClient.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,16 +23,23 @@ namespace AuctionClient
     {
         private string sellerName;
         private int sellerCash;
+        public AukzionContractClient seller;
+        public AuctionViewModel viewmodel { get; set; }
 
         public SellerWindow()
         {
+            viewmodel = new AuctionViewModel();
             InitializeComponent();
+            sellerName = "NoName";
+            sellerCash = 0;
         }
 
         public SellerWindow(string sellerName, int sellerCash)
         {
             this.sellerName = sellerName;
             this.sellerCash = sellerCash;
+            viewmodel = new AuctionViewModel();
+
 
         }
 
